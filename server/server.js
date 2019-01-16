6044d22901569b2e184047133e6eb36334a8f9ca
Stefan <38556377+StefanRadulovic@users.noreply.github.com>
2019-01-16 17:01:25 +0100

    location added

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage,generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
@@ -23,11 +23,10 @@ io.on('connection', (socket) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server.');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+  });
+
+  socket.on('createLocationMessage',(coords)=>{
+    io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude))
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,9 +1,20 @@
 var generateMessage = (from, text) => {
-  return {
-    from,
-    text,
-    createdAt: new Date().getTime()
-  };
+    return {
+        from,
+        text,
+        createdAt: new Date().getTime()
+    };
 };
 
-module.exports = {generateMessage};
+var generateLocationMessage = (from, lat, lng) => {
+    console.log('location',`https://www.google.com/maps?q=${lat},${lng}`);
+    return {
+        from,
+        url: `https://www.google.com/maps?q=${lat},${lng}`,
+        createdAt: new Date().getTime()
+    }
+};
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+};
