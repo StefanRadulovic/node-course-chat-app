884c00f67281a3bc380c3c905ded90ed0d11549e
Stefan <38556377+StefanRadulovic@users.noreply.github.com>
2019-01-16 10:27:26 +0100

    newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -5,19 +5,30 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
-io.on('connection',(socket)=>{
-    console.log('New user connected');
 
-    socket.on('disconnect',()=>{
-        console.log('User was disconnected')
-    })
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
+
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}`)
+  console.log(`Server is up on ${port}`);
 });
-
